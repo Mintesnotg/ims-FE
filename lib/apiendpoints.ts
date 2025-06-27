2130ede88602428d3efca3661f2870ea46f7dfef
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2025-06-27 12:39:25 +0300

    preparingloginpage

---
+++ lib/apiendpoints.ts
@@ -0,0 +1,10 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+
+export const  USER_ACCOUNT_ENDPOINTS= {
+
+    Login : `${BASE_URL}/api/v1/UserAccout/Login`,
+    Register: `${BASE_URL}/api/v1/UserAccout/Register`
+
+
+}
